82672b43bc30a28a99cb844a23e1aabf754b911f
avuthusiva <asvr410@gmail.com>
2026-01-25 21:23:42 +0530

    Added new files

--- my_work/prac_31.py
+++ my_work/prac_31.py
@@ -13,4 +13,6 @@ session.use_schema("MY_SCHEMA")
 df = session.table("employees")
 df_pivot = df.pivot(col("DEPARTMENT"),["PRESIDENCE", "IT", "FINANCE"]).sum(col("SALARY"))
 df_pivot.show()
+last_id = df_pivot.select(last_query_id()).collect()[0][0]
+print(f"Last Query ID: {last_id}")
 session.close()
