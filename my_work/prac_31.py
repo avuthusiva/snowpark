d0aef9fbc64da6b3436ac6adababae195947a883
avuthusiva <asvr410@gmail.com>
2026-01-25 19:31:21 +0530

    Added new files

---
+++ my_work/prac_31.py
@@ -0,0 +1,16 @@
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import *
+from snowflake.snowpark.types import *
+import json
+
+with open('config.json') as f:
+    connection_parameters = json.load(f)
+session = Session.builder.configs(connection_parameters).create()
+session.use_role("ACCOUNTADMIN")
+session.use_warehouse("MY_WAREHOUSE")
+session.use_database("MY_DB")
+session.use_schema("MY_SCHEMA")
+df = session.table("employees")
+df_pivot = df.pivot(col("DEPARTMENT"),["PRESIDENCE", "IT", "FINANCE"]).sum(col("SALARY"))
+df_pivot.show()
+session.close()
