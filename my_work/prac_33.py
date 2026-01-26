82d44101468e374719bd3a7fc8715d7a54fbd666
avuthusiva <asvr410@gmail.com>
2026-01-26 15:08:44 +0530

    Added new files to github

--- my_work/prac_33.py
+++ my_work/prac_33.py
@@ -1,3 +1,4 @@
+from numpy import select
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import *
 from snowflake.snowpark.types import *
@@ -15,5 +16,15 @@ stage_file = session.file.put(file_name,'@int_stage/files/json', auto_compress=F
 print(stage_file)
 df = session.read.options({"type":'json','strip_outer_array':True}).json('@int_stage/files/json/car-sales.json')
 df = df.select(col("$1").alias("CAR_SALES_DATA"))
-df.select(col("CAR_SALES_DATA")["customer"]).show()
+df.select(col("CAR_SALES_DATA")["customer"],col("CAR_SALES_DATA")["date"], \
+          col("CAR_SALES_DATA")["dealership"],col("CAR_SALES_DATA")["salesperson"], \
+            col("CAR_SALES_DATA")["vehicle"]).show()
+df.join_table_function("flatten", col("CAR_SALES_DATA")["customer"]) \
+    .select(col("value")["address"].as_("address"), \
+            col("value")["name"].as_("name"), \
+            col("value")["phone"].as_("phone"), \
+            col("CAR_SALES_DATA")["date"].as_("date"),
+            col("CAR_SALES_DATA")["dealership"].as_("dealership"),
+            col("CAR_SALES_DATA")["salesperson"].as_("salesperson"),
+            col("CAR_SALES_DATA")["vehicle"].as_("vehicle")).show()
 session.close()
