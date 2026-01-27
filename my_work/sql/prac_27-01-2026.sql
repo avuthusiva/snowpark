277c3b7b37f97808050d0e85211943c58d4e26ce
avuthusiva <asvr410@gmail.com>
2026-01-27 13:07:06 +0530

    Added new changes to file

--- my_work/sql/prac_27-01-2026.sql
+++ my_work/sql/prac_27-01-2026.sql
@@ -31,3 +31,44 @@ begin
 end;
 $$; 
 call pr_add_numbers(5, 10);
+execute immediate 
+$$
+declare
+    first_name varchar default 'siva';
+    last_name varchar default 'reddy';
+    full_name varchar;
+begin
+    let middle_name := 'vardhana';
+    full_name := first_name || ' ' || middle_name || ' ' || last_name;
+    --select :first_name || ' ' || :middle_name || ' ' || :last_name into :full_name from dual;
+    return full_name;
+end;
+$$;
+set age = 34;
+select $age from dual;
+execute immediate
+$$
+declare 
+    out_str varchar := '';
+begin
+    for i in 1 to 20 
+    loop
+        out_str := out_str || ' ' || i;
+    end loop;
+    return out_str;
+end;
+$$;
+execute immediate
+$$
+declare
+    out_str varchar := '';
+    cnt int := 1;
+begin
+    while (cnt <=20)
+    loop
+        out_str := out_str || ' ' || cnt;
+        cnt := cnt + 1;
+    end loop;
+    return trim(out_str);
+end;
+$$;
